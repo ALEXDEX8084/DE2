eb785acf803485a3f5d000bfcd950382cc7e42fe
meleg <meleg@DESKTOP-53UTIB1>
2024-06-16 21:37:09 +0300

    Добавьте файлы проекта.

---
+++ Pages/AddEditPage.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfApp1.Classes;
+
+namespace WpfApp1.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для AddEditPage.xaml
+    /// </summary>
+    public partial class AddEditPage : Page
+    {
+        private Orders currentuser = new Orders();
+
+        public AddEditPage(Orders SelectedUser)
+        {
+            InitializeComponent();
+            if (SelectedUser != null)
+                currentuser = SelectedUser;
+            DataContext = currentuser;
+            cmbvoxname.ItemsSource = EkzamBDEntities.GetContext().Mans.ToList();
+            cmbvoxname.SelectedValue = "ID_Man";
+            cmbvoxname.DisplayMemberPath = "Name";
+        }
+
+        private void Save_Click(object sender, RoutedEventArgs e)
+        {
+            StringBuilder error = new StringBuilder();
+            if (string.IsNullOrWhiteSpace(currentuser.DateOrder.ToString()))
+                error.AppendLine("Karapulka");
+            if(error.Length > 0)
+            {
+                MessageBox.Show(error.ToString());
+                return;
+            }
+            if (currentuser.ID_Order == 0)
+                EkzamBDEntities.GetContext().Orders.Add(currentuser);
+            try
+            {
+                EkzamBDEntities.GetContext().SaveChanges();
+                MessageBox.Show("pobeda");
+                this.NavigationService.Navigate(new Pages.StartPage());
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+
+        }
+    }
+}
---
+++ Pages/StartPage.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfApp1.Classes;
+
+namespace WpfApp1.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для StartPage.xaml
+    /// </summary>
+    public partial class StartPage : Page
+    {
+        public StartPage()
+        {
+            InitializeComponent();
+            ListOrders.ItemsSource = EkzamBDEntities.GetContext().Orders.ToList();
+        }
+
+        private void Search_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            ListOrders.ItemsSource = EkzamBDEntities.GetContext().Orders.Where(x => x.Mans.Name.Contains(Search.Text)).ToList();
+        }
+
+        private void a1_Checked(object sender, RoutedEventArgs e)
+        {
+            ListOrders.ItemsSource = EkzamBDEntities.GetContext().Orders.OrderBy(x => x.Mans.Name).ToList();
+        }
+
+        private void a2_Checked(object sender, RoutedEventArgs e)
+        {
+            ListOrders.ItemsSource = EkzamBDEntities.GetContext().Orders.OrderByDescending(x => x.Mans.Name).ToList();
+        }
+
+        private void Add_Click(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.Navigate(new Pages.AddEditPage(null));
+        }
+
+        private void Delete_Click(object sender, RoutedEventArgs e)
+        {
+            var personforremoving = ListOrders.SelectedItems.Cast<Orders>().ToList();
+            var resMessage = MessageBox.Show("Удалить запись?", "Удаление", MessageBoxButton.YesNo, MessageBoxImage.Question);
+            if(resMessage == MessageBoxResult.Yes)
+            {
+                try { 
+                EkzamBDEntities.GetContext().Orders.RemoveRange(personforremoving);
+                EkzamBDEntities.GetContext().SaveChanges();
+                MessageBox.Show("Udaleno");
+                ListOrders.ItemsSource = EkzamBDEntities.GetContext().Orders.ToList();
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show(ex.ToString());
+                }
+            }
+           
+        }
+
+        private void Edit_Click(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.Navigate(new Pages.AddEditPage((Orders)ListOrders.SelectedItem));
+        }
+    }
+}
